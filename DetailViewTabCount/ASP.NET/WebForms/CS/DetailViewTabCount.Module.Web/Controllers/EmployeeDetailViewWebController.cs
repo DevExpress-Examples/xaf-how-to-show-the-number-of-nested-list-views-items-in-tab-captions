5094f8de8b96d74c7a2e7e988d63af612f779641
dxstanley <stanislav.gurov@devexpress.com>
2020-10-13 19:15:27 +0300

    update Web module

--- DetailViewTabCount/ASP.NET/WebForms/CS/DetailViewTabCount.Module.Web/Controllers/EmployeeDetailViewWebController.cs
+++ DetailViewTabCount/ASP.NET/WebForms/CS/DetailViewTabCount.Module.Web/Controllers/EmployeeDetailViewWebController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using DetailViewTabCount.Module.Helpers;
 using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Web;
 using DevExpress.ExpressApp.Web.Editors.ASPx;
 using DevExpress.ExpressApp.Web.Layout;
 using DevExpress.ExpressApp.Web.Utils;
@@ -10,7 +11,7 @@ using DevExpress.Web;
 
 namespace DetailViewTabCount.Module.Web.Controllers
 {
-    public partial class EmployeeDetailViewWebController : ViewController<DetailView>
+    partial class EmployeeDetailViewWebController : ViewController<DetailView>
     {
         private ASPxPageControl pageControl;
 
@@ -25,11 +26,43 @@ namespace DetailViewTabCount.Module.Web.Controllers
             View.DelayedItemsInitialization = false;
             View.CurrentObjectChanged += View_CurrentObjectChanged;
             ((WebLayoutManager)View.LayoutManager).PageControlCreated += EmployeeDetailViewWebController_PageControlCreated;
+            WebWindow.CurrentRequestWindow.PagePreRender += CurrentRequestWindow_PagePreRender;
+            View.CustomizeViewItemControl<ListPropertyEditor>(this, (editor) =>
+            {
+                editor.ListView.ControlsCreated += ListView_ControlsCreated;
+            });
         }
+
+        private void ListView_ControlsCreated(object sender, EventArgs e)
+        {
+            ASPxGridListEditor gridListEditor = ((ListView)sender).Editor as ASPxGridListEditor;
+            if (gridListEditor != null)
+            {
+                if (gridListEditor.Grid != null)
+                {
+                    gridListEditor.Grid.BeforeGetCallbackResult += Grid_BeforeGetCallbackResult; ;
+                }
+            }
+        }
+
+        private void Grid_BeforeGetCallbackResult(object sender, EventArgs e)
+        {
+            UpdatePageControl(pageControl);
+        }
+
+        private void CurrentRequestWindow_PagePreRender(object sender, EventArgs e)
+        {
+            if (pageControl != null) UpdatePageControl(pageControl);
+        }
+
         protected override void OnDeactivated()
         {
             View.CurrentObjectChanged -= View_CurrentObjectChanged;
             ((WebLayoutManager)View.LayoutManager).PageControlCreated -= EmployeeDetailViewWebController_PageControlCreated;
+            WebWindow.CurrentRequestWindow.PagePreRender -= CurrentRequestWindow_PagePreRender;
+            View.CustomizeViewItemControl<ListPropertyEditor>(this, (editor) => {
+                editor.ListView.ControlsCreated -= ListView_ControlsCreated;
+            });
             base.OnDeactivated();
         }
         protected override void OnViewControlsDestroying()
@@ -44,7 +77,6 @@ namespace DetailViewTabCount.Module.Web.Controllers
             {
                 pageControl = e.PageControl;
                 pageControl.ClientInstanceName = "pageControl";
-                pageControl.Load += (s, args) => UpdatePageControl(e.PageControl);
             }
         }
         private void View_CurrentObjectChanged(object sender, EventArgs e)
@@ -59,19 +91,23 @@ namespace DetailViewTabCount.Module.Web.Controllers
             foreach (TabPage tab in pageControl.TabPages)
             {
                 tab.Text = DetailViewControllerHelper.ClearItemCountInTabCaption(tab.Text);
-                var listPropertyEditor = View.FindItem(tab.Text.Replace(" ", "")) as ListPropertyEditor;
+                var listPropertyEditor = View.FindItem(tab.Name) as ListPropertyEditor;
                 if (listPropertyEditor != null)
                 {
                     var count = listPropertyEditor.ListView.CollectionSource.GetCount();
+                    tab.TabStyle.Font.Bold = false;
                     if (count > 0)
                     {
                         tab.TabStyle.Font.Bold = true;
                         tab.Text += " (" + count + ")";
-                        if (listPropertyEditor.ListView.Editor is ASPxGridListEditor editor && editor.Grid != null)
-                        {
-                            editor.Grid.JSProperties["cpCaption"] = tab.Text;
-                            ClientSideEventsHelper.AssignClientHandlerSafe(editor.Grid, "EndCallback", $"function(s, e) {{var tab = {pageControl.ClientInstanceName}.GetTabByName('{tab.Name}'); tab.SetText(s.cpCaption); delete s.cpCaption;}}", nameof(EmployeeDetailViewWebController));
-                        }
+                    }
+                    if (listPropertyEditor.ListView.Editor is ASPxGridListEditor editor && editor.Grid != null)
+                    {
+                        editor.Grid.JSProperties["cpCaption"] = tab.Text;
+                        ClientSideEventsHelper.AssignClientHandlerSafe(editor.Grid, "EndCallback", $"function(s, e) {{ " +
+                            $"var tab = {pageControl.ClientInstanceName}.GetTabByName('{tab.Name}');" +
+                            $"tab.SetText(s.cpCaption);" +
+                            $"delete s.cpCaption}}", nameof(EmployeeDetailViewWebController));
                     }
                 }
             }
