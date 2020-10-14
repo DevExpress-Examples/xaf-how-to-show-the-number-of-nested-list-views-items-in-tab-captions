f1031f5bb34e21997ddd122b98e2b153c8036523
dxstanley <stanislav.gurov@devexpress.com>
2020-10-14 13:07:01 +0300

    update VB examples

--- DetailViewTabCount/ASP.NET/WebForms/VB/DetailViewTabCountVB.Module.Web/Controllers/EmployeeDetailViewWebController.vb
+++ DetailViewTabCount/ASP.NET/WebForms/VB/DetailViewTabCountVB.Module.Web/Controllers/EmployeeDetailViewWebController.vb
@@ -2,6 +2,7 @@
 Imports System.Collections.Generic
 Imports DevExpress.ExpressApp
 Imports DevExpress.ExpressApp.Editors
+Imports DevExpress.ExpressApp.Web
 Imports DevExpress.ExpressApp.Web.Editors.ASPx
 Imports DevExpress.ExpressApp.Web.Layout
 Imports DevExpress.ExpressApp.Web.Utils
@@ -22,11 +23,19 @@ Namespace DetailViewTabCountVB.Module.Web.Controllers
             View.DelayedItemsInitialization = False
             AddHandler View.CurrentObjectChanged, AddressOf View_CurrentObjectChanged
             AddHandler(CType(View.LayoutManager, WebLayoutManager)).PageControlCreated, AddressOf EmployeeDetailViewWebController_PageControlCreated
+            AddHandler WebWindow.CurrentRequestWindow.PagePreRender, AddressOf CurrentRequestWindow_PagePreRender
+        End Sub
+
+        Private Sub CurrentRequestWindow_PagePreRender(ByVal sender As Object, ByVal e As EventArgs)
+            If pageControl IsNot Nothing Then
+                UpdatePageControl(pageControl)
+            End If
         End Sub
 
         Protected Overrides Sub OnDeactivated()
             RemoveHandler View.CurrentObjectChanged, AddressOf View_CurrentObjectChanged
             RemoveHandler(CType(View.LayoutManager, WebLayoutManager)).PageControlCreated, AddressOf EmployeeDetailViewWebController_PageControlCreated
+            RemoveHandler WebWindow.CurrentRequestWindow.PagePreRender, AddressOf CurrentRequestWindow_PagePreRender
             MyBase.OnDeactivated()
         End Sub
 
@@ -39,7 +48,6 @@ Namespace DetailViewTabCountVB.Module.Web.Controllers
             If e.Model.Id = "Tabs" Then
                 pageControl = e.PageControl
                 pageControl.ClientInstanceName = "pageControl"
-                AddHandler pageControl.Load, Sub(s, args) UpdatePageControl(e.PageControl)
             End If
         End Sub
 
@@ -52,32 +60,26 @@ Namespace DetailViewTabCountVB.Module.Web.Controllers
         Private Sub UpdatePageControl(ByVal pageControl As ASPxPageControl)
             For Each tab As TabPage In pageControl.TabPages
                 tab.Text = DetailViewControllerHelper.ClearItemCountInTabCaption(tab.Text)
-                Dim listPropertyEditor = TryCast(View.FindItem(tab.Text.Replace(" ", "")), ListPropertyEditor)
+                Dim listPropertyEditor = TryCast(View.FindItem(tab.Name), ListPropertyEditor)
 
                 If listPropertyEditor IsNot Nothing Then
                     Dim count = listPropertyEditor.ListView.CollectionSource.GetCount()
 
                     If count > 0 Then
-                        tab.TabStyle.Font.Bold = True
                         tab.Text += " (" & count & ")"
-
-                        If TypeOf listPropertyEditor.ListView.Editor Is ASPxGridListEditor AndAlso DirectCast(listPropertyEditor.ListView.Editor, ASPxGridListEditor).Grid IsNot Nothing Then
-                            Dim editor = DirectCast(listPropertyEditor.ListView.Editor, ASPxGridListEditor)
-                            editor.Grid.JSProperties("cpCaption") = tab.Text
-                            ClientSideEventsHelper.AssignClientHandlerSafe(editor.Grid, "EndCallback", $"function(s, e) {{var tab = {pageControl.ClientInstanceName}.GetTabByName('{tab.Name}'); tab.SetText(s.cpCaption); delete s.cpCaption;}}", NameOf(EmployeeDetailViewWebController))
-                        End If
+                    End If
+                    If TypeOf listPropertyEditor.ListView.Editor Is ASPxGridListEditor AndAlso DirectCast(listPropertyEditor.ListView.Editor, ASPxGridListEditor).Grid IsNot Nothing Then
+                        Dim editor = DirectCast(listPropertyEditor.ListView.Editor, ASPxGridListEditor)
+                        editor.Grid.JSProperties("cpCaption") = tab.Text
+                        ClientSideEventsHelper.AssignClientHandlerSafe(editor.Grid, "EndCallback", $"function(s, e) {{ 
+                        if (!s.cpCaption) return;
+                        var tab = {pageControl.ClientInstanceName}.GetTabByName('{tab.Name}'); 
+                        tab.SetText(s.cpCaption); 
+                        delete s.cpCaption;}}", NameOf(EmployeeDetailViewWebController))
                     End If
                 End If
             Next
         End Sub
-
-        Private Class CSharpImpl
-            <Obsolete("Please refactor calling code to use normal Visual Basic assignment")>
-            Shared Function __Assign(Of T)(ByRef target As T, value As T) As T
-                target = value
-                Return value
-            End Function
-        End Class
     End Class
 
 End Namespace
