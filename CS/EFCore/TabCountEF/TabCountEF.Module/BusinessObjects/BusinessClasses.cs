4b1c25ffda02a786cd568f4e836978c2b7cf23e4
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-20 16:43:15 +0400

    ef

---
+++ CS/EFCore/TabCountEF/TabCountEF.Module/BusinessObjects/BusinessClasses.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.EF;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySolution.Module.BusinessObjects;
+[DefaultClassOptions]
+public class Contact : BaseObject {
+    public virtual string FirstName { get; set; }
+    public virtual string LastName { get; set; }
+    public virtual int Age { get; set; }
+    public virtual DateTime BirthDate { get; set; }
+
+    public virtual ObservableCollection<MyTask> MyTasks { get; set; } = new ObservableCollection<MyTask>();
+    public virtual ObservableCollection<Car> Cars { get; set; } = new ObservableCollection<Car>();
+}
+[DefaultClassOptions]
+public class MyTask : BaseObject {
+    public virtual string Subject{get;set;}
+    public virtual Contact AssignedTo{ get; set; }
+}
+[DefaultClassOptions]
+public class Car : BaseObject {
+    public virtual string CarName { get; set; }
+    public virtual Contact AssignedTo { get; set; }
+}
+
+
---
+++ CS/EFCore/TabCountEF/TabCountEF.Module/BusinessObjects/TabCountEFDbContext.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using MySolution.Module.BusinessObjects;
+
+namespace TabCountEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class TabCountEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<TabCountEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new TabCountEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class TabCountEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<TabCountEFEFCoreDbContext> {
+	public TabCountEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<TabCountEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=TabCountEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new TabCountEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(TabCountEFContextInitializer))]
+public class TabCountEFEFCoreDbContext : DbContext {
+	public TabCountEFEFCoreDbContext(DbContextOptions<TabCountEFEFCoreDbContext> options) : base(options) {
+	}
+    //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+    public DbSet<MyTask> MyTasks { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Car> Cars { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+    }
+}
